3f082fd64b4fe352f9e79d7f2048389f531dad92
PixerJason <109621806+PixerJason@users.noreply.github.com>
2023-02-11 17:24:40 -0500

    Update ip.py

--- ip.py
+++ ip.py
@@ -4,7 +4,6 @@ try:
     from configparser import ConfigParser
     import sys
     import time
-    import platform
     from prettytable import PrettyTable
     import random
 except:
@@ -16,20 +15,13 @@ except:
 
 #Dah mamae nmechoka
 conf = ConfigParser()
-ghostcracker = PrettyTable()
+x = PrettyTable()
 
 color = ['\x1b[38;5;118m','\x1b[38;5;33m','\033[0;35m','\033[0;36m','\033[0;32m','\033[0;35m''\e[0;95m','\e[35m','\033[1;33m']
  
 pixer = random.choice(color)    
 
-def safisha():
-    if platform == 'win32':
-        os.system('cls')
-    else:
-        os.system('clear')
-
-
-ghostcracker.field_names = ["city","country","country_name","postal","org","ip","loc","region","timezone","latitude","longitude"]
+x.field_names = ['NAME','DATA']
 
 conf.read("vitu/config.ini")
 
@@ -59,15 +51,24 @@ def ass(num):
     try: 
         handler = ipinfo.getHandler(conf.get("TOKEN","access_token"))
         details = handler.getDetails(num)
-        ghostcracker.add_row([f"{details.city}",f"{details.country}",f"{details.country_name}",f"{details.postal}",f"{details.org}",f"{details.ip}",f"{details.loc}",f"{details.region}",f"{details.timezone}",f"{details.latitude}",f"{details.longitude}"])
-        print(ghostcracker)
+        x.add_row(["City",f"{details.city}"])
+        x.add_row(["Country",f"{details.country}"])
+        x.add_row(["Country Name",f"{details.country_name}"])
+        x.add_row(["Postal",f"{details.postal}"])
+        x.add_row(["Org",f"{details.org}"])
+        x.add_row(["IP",f"{details.ip}"])
+        x.add_row(["Location",f"{details.loc}"])
+        x.add_row(["Region",f"{details.region}"])
+        x.add_row(["TimeZone",f"{details.timezone}"])
+        x.add_row(["Latitude",f"{details.latitude}"])
+        x.add_row(["Longitude",f"{details.longitude}"])
+        print(x)
         typing("Telegram: https://t.me/PixerJason")
     except:
         fucktoken()
 
 def main():
     try:
-      safisha()
       typing("\t\t\t0\t\tCoded By Pixer Jason!")
       banner()
       num = input("Enter The Fuckn IP: ")
